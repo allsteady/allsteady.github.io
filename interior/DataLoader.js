057a9d3aed6e15a6cc7b3df6011ff784e9d38611
allsteady <allsteady@gmail.com>
2015-07-15 11:14:33 +0900

    가구

--- interior/DataLoader.js
+++ interior/DataLoader.js
@@ -6,11 +6,24 @@ var DataLoader = function () {
 	var imgPath = "./images/";
 	this.dataSet = {};
 	this.addData( "library", "서재_00", imgPath + "library_000.jpg" );
-	this.addData( "library", "서재_01", imgPath + "library_004.png" );
-	this.addData( "library", "서재_02", imgPath + "library_002.jpg" );
-	this.addData( "library", "서재_03", imgPath + "library_003.jpg" );
-	this.addData( "library", "서재_04", imgPath + "library_005.jpg" );
-	this.addData( "library", "서재_05", imgPath + "library_001.jpg" );
+	this.addData( "library", "서재_01", imgPath + "library_005.jpg" );
+	this.addData( "library", "서재_02", imgPath + "library_004.png" );
+	this.addData( "library", "서재_03", imgPath + "library_002.jpg" );
+	this.addData( "library", "서재_04", imgPath + "library_003.jpg" );
+
+	this.addData( "fur", "클라인3 거실장 1600", imgPath + "fur_000.jpg", "너비 160 x 깊이 40 x 높이 53" );
+//	this.addData( "fur", "소파", imgPath + "fur_001.jpg", "너비 160 x 깊이 40 x 높이 53" );
+	this.addData( "fur", "클라인 애쉬 소파테이블", imgPath + "fur_002.jpg", "너비 95 x 깊이 40 x 높이 48" );
+	this.addData( "fur", "비트윈 식탁 세트", imgPath + "fur_003.jpg", "너비 120 x 깊이 75 x 높이 75" );
+	this.addData( "fur", "폴린 침대 Q", imgPath + "fur_004.jpg", "너비 120 x 깊이 214.7 x 높이 104.7" );
+	this.addData( "fur", "폴린 오크 4단 서랍장", imgPath + "fur_005.jpg", "너비 80 x 깊이 78.5 x 높이 95.3" );
+	this.addData( "fur", "폴린 협탁", imgPath + "fur_006.png", "" );
+	this.addData( "fur", "폴린 테이블램프", imgPath + "fur_007.jpg");
+
+	this.addData( "fur", "지플랜 B3 책장", imgPath + "fur_008.jpg", "너비 80 x 깊이 30 x 높이 124" );
+	this.addData( "fur", "팀버나인 책장", imgPath + "fur_009.jpg", "너비 119 x 깊이 30 x 높이 98.6" );
+	this.addData( "fur", "팀버나인 책상", imgPath + "fur_010.jpg", "너비 160 x 깊이 85 x 높이 74");
+	this.addData( "fur", "폴린  붙박이 장", imgPath + "fur_011.jpg", "");
 
 }
 
